e4b3d9190e1842ba9014187c2350729e57c81ca6
Tiago Vasconcelos <talvasconcelos@gmail.com>
2025-05-14 16:55:18 +0100

    feat: print receipt

---
+++ static/components/receipt.js
@@ -0,0 +1,86 @@
+window.app.component('receipt', {
+  name: 'receipt',
+  props: ['data'],
+  data() {
+    return {}
+  },
+  computed: {
+    cartSubtotal() {
+      let subtotal = 0
+      this.data.extra.details.items.forEach(item => {
+        subtotal += item.price * item.quantity
+      })
+      return subtotal
+    },
+    cartTotal() {
+      let total = 0
+      this.data.extra.details.items.forEach(item => {
+        total += item.price * item.quantity
+      })
+      if (this.data.extra.details.taxIncluded) {
+        return total
+      }
+      return total + this.data.extra.details.taxValue
+    },
+    exchangeRateInfo() {
+      return `Rate (sat/${this.data.extra.details.currency}): ${this.data.extra.details.exchangeRate.toFixed(2)}`
+    },
+    formattedDate() {
+      return LNbits.utils.formatDateString(this.data.created_at)
+    }
+  },
+  methods: {},
+  created() {},
+  template: `
+    <div class="q-pa-md">
+    <div class="text-center q-mb-xl">
+    <p class='text-h6 text-uppercase'>Receipt</p>
+    <p class=''><span v-text="formattedDate"></span></p>
+    <p class=''><span v-text="exchangeRateInfo"></span></p>
+    </div>
+    <q-table
+    class="q-mb-xl"
+      :rows="data.extra.details.items"
+      :columns="[
+          { name: 'title', label: 'Item', field: 'title' },
+          { name: 'formattedPrice', label: 'Unit Price', field: 'formattedPrice', align: 'right' },
+          { name: 'quantity', label: 'Quantity', field: 'quantity' },
+        ]"
+      row-key="title"
+        hide-bottom
+    >
+    </q-table>
+    <div class="q-mb-xl q-gutter-md">
+      <div class="row">
+        <div class="col-6">Subtotal</div>
+        <div class="col-6 text-right">
+        <span v-text="cartSubtotal"></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">Tax</div>
+        <div class="col-6 text-right">
+        <span v-text="data.extra.details.taxValue.toFixed(2)"></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">Total</div>
+        <div class="col-6 text-right">
+        <span v-text="cartTotal.toFixed(2)"></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">Total (sats)</div>
+        <div class="col-6 text-right">
+        <span v-text="data.extra.amount"></span>
+        </div>
+      </div>
+    </div>
+    <div class="text-center q-mb-xl">
+    <p class='text-h6 text-uppercase'>
+    Thank you for your purchase! 
+    </p>
+    </div>
+  </div>
+    `
+})
