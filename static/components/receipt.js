3796b5a30c9d42d4ff1f7c568747c40a50200725
Arc <ben@arc.wales>
2026-01-12 13:57:21 +0000

    scrollbar fix

--- static/components/receipt.js
+++ static/components/receipt.js
@@ -92,28 +92,50 @@ window.app.component('receipt', {
       <span v-text="data.business_vat_id"></span>
     </div>
     <q-table v-if="data.extra.details.items && data.extra.details.items.length > 0"
+      dense
+      class="q-ma-none"
       :hide-pagination="true"
       :rows-per-page-options="[0]"  
       :rows="data.extra.details.items"
+      class="q-pa-none text-caption"
       :columns="[
           { name: 'title', label: 'Item', field: 'title' },
-          { name: 'formattedPrice', label: 'Unit Price', field: 'formattedPrice', align: 'right' },
-          { name: 'quantity', label: 'Quantity', field: 'quantity' },
+          { name: 'formattedPrice', label: 'Price', field: 'formattedPrice', align: 'right' },
+          { name: 'quantity', label: 'Qty', field: 'quantity' },
         ]"
       row-key="title"
         hide-bottom
     >
+      <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            <span class="q-pa-none text-subtitle2 text-no-wrap" v-text="col.label"></span>
+          </q-th>
+        </q-tr>
+      </template>
       <template v-slot:body="props">
         <q-tr :props="props">
-          <q-td key="title" :props="props">
-            <div v-text="props.row.title"></div>
-            <div v-if="props.row.note" class="text-caption" v-text="props.row.note"></div>
+          <q-td key="title" :props="props" class="q-pa-none">
+            <div class="text-subtitle2" v-text="props.row.title"></div>
+            <div
+              v-if="props.row.note"
+              class="text-subtitle2 text-italic"
+              v-text="props.row.note"
+            ></div>
           </q-td>
-          <q-td key="formattedPrice" :props="props" class="text-right">
-            <span v-text="props.row.formattedPrice"></span>
+          <q-td
+            key="formattedPrice"
+            :props="props"
+            class="q-pa-none text-right text-no-wrap"
+          >
+            <span class="text-subtitle2 text-no-wrap" v-text="props.row.formattedPrice"></span>
           </q-td>
-          <q-td key="quantity" :props="props">
-            <span v-text="props.row.quantity"></span>
+          <q-td key="quantity" :props="props" class="q-pa-none">
+            <span class="text-subtitle2 text-no-wrap" v-text="props.row.quantity"></span>
           </q-td>
         </q-tr>
       </template>
